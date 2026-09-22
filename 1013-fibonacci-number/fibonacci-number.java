41d69a45636ec0b5e11660bf02ccc6b711e28700
Samarth <113123386+luciferthestar302@users.noreply.github.com>
2026-09-22 20:16:27 +0530

    Time: 10 ms (25.72%) | Memory: 42.1 MB (52.49%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -4,9 +4,7 @@ class Solution {
     }
     private int fibo(int n){
         //base condition
-        if(n==0) return 0;
-
-        if(n==1) return 1;
+        if(n<=1) return n;
         //recursive relation
         return fibo(n-1)+fibo(n-2);
     }
